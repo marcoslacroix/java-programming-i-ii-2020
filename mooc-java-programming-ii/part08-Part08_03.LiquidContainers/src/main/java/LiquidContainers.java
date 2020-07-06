8f2f47d97440ea50d86cfbfda10b83c9742565b4
marcoslacroix <marcoslacroix@gmail.com>
2020-07-05 21:18:12 -0300

    added part8_03

--- mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
+++ mooc-java-programming-ii/part08-Part08_03.LiquidContainers/src/main/java/LiquidContainers.java
@@ -1,20 +1,53 @@
 
 import java.util.Scanner;
 
-public class LiquidContainers {
-
+public class LiquidContainers { 
+    
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+        
+        int first = 0;
+        int second = 0;       
 
         while (true) {
-            System.out.print("> ");
-
+            System.out.println("First: " + first + "/100");
+            System.out.println("Second: " + second + "/100");
             String input = scan.nextLine();
-            if (input.equals("quit")) {
+            if (input.equals("quit")) {                
                 break;
+            }            
+            String[] parts = input.split(" ");
+            String command = parts[0];
+            int amount = Integer.valueOf(parts[1]);  
+            
+            if (amount < 0) {
+                continue;
             }
-
+            if (command.equals("add")) {
+                first += amount;
+                if (first > 100) {
+                    first = 100;
+                }
+            }   
+            else if (command.equals("move")) {
+                if (amount <= first) {
+                    first -= amount;
+                    second += amount;
+                } else {
+                    second += first;
+                    first = 0;
+                }
+                if (second > 100) {
+                    second = 100;
+                }
+                
+            }
+            else if (command.equals("remove")) {
+                second -= amount;
+                if (second < 0) {
+                    second = 0;
+                }
+            }          
         }
     }
 
